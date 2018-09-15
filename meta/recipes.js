e23f5b1a24e1cd4aaba485a10659d4f098b236ac
David Keller <dev@davidkeller.me>
2018-09-15 22:03:40 +0200

    Update data

--- meta/recipes.js
+++ meta/recipes.js
@@ -1,51 +1,128 @@
 const recipes = {
   "fooby.ch/en/recipes/14229/lasagne-al-forno": {
-    "Name": "Lasagne",
+    Name: "Lasagne",
+    recipe_url:
+      "https://fooby.ch/en/recipes/14229/lasagne-al-forno?startAuto1=4",
+    picture_url: "https://recipecontent.fooby.ch/14229_3-2_1560-1040.jpg",
 
-    "ingredients": [
+    ingredients: [
       {
-        "Product": "Beef",
-        "kg": 0.15
+        Product: "Beef",
+        kg: 0.15
       },
       {
-        "Product": "Onions",
-        "kg": 0.044
+        Product: "Onions",
+        kg: 0.044
       },
       {
-        "Product": "Tomatoes",
-        "kg": 0.015
+        Product: "Tomatoes",
+        kg: 0.015
       },
       {
-        "Product": "Carrots",
-        "kg": 0.05
+        Product: "Carrots",
+        kg: 0.05
       },
       {
-        "Product": "Celery",
-        "kg": 0.05
+        Product: "Celery",
+        kg: 0.05
       },
       {
-        "Product": "Red wine",
-        "kg": 0.075
+        Product: "Red wine",
+        kg: 0.075
       },
       {
-        "Product": "Butter",
-        "kg": 0.012
+        Product: "Butter",
+        kg: 0.012
       },
       {
-        "Product": "White Flour",
-        "kg": 0.01
+        Product: "White Flour",
+        kg: 0.01
       },
       {
-        "Product": "Whole Milk",
-        "kg": 0.15
+        Product: "Whole Milk",
+        kg: 0.15
       },
       {
-        "Product": "Dry Pasta",
-        "kg": 0.125
+        Product: "Dry Pasta",
+        kg: 0.125
       },
       {
-        "Product": "Cheese slices",
-        "kg": 0.02
+        Product: "Cheese slices",
+        kg: 0.02
+      }
+    ]
+  },
+  "fooby.ch/en/recipes/16427/provencale-style-rack-of-lamb-with-ratatouille": {
+    Name: "Provencale-Style Rack of Lamb with Ratatouille",
+    recipe_url:
+      "https://fooby.ch/en/recipes/16427/provencale-style-rack-of-lamb-with-ratatouille?startAuto1=0",
+    picture_url: "https://recipecontent.fooby.ch/16427_3-2_1560-1040.jpg 1x",
+
+    ingredients: [
+      {
+        Product: "Olive oil",
+        kg: 0.04
+      },
+      {
+        Product: "Onion",
+        kg: 0.0208
+      },
+      {
+        Product: "Garlic",
+        kg: 0.0075
+      },
+      {
+        Product: "Aubergine",
+        kg: 0.085
+      },
+      {
+        Product: "Pepper",
+        kg: 0.0388
+      },
+      {
+        Product: "Courgette",
+        kg: 0.0525
+      },
+      {
+        Product: "Tomatoes",
+        kg: 0.05
+      },
+      {
+        Product: "Lamb",
+        kg: 0.175
+      }
+    ]
+  },
+  "fooby.ch/en/recipes/12728/chocolate-dessert-with-strawberries": {
+    Name: "Chocolate Dessert with Strawberries",
+    recipe_url:
+      "https://fooby.ch/en/recipes/12728/chocolate-dessert-with-strawberries?startAuto1=4",
+    picture_url: "https://recipecontent.fooby.ch/12728_3-2_1560-1040.jpg 1x",
+
+    ingredients: [
+      {
+        Product: "Whole Milk",
+        kg: 0.075
+      },
+      {
+        Product: "Cream",
+        kg: 0.1
+      },
+      {
+        Product: "Eggs",
+        kg: 0.0315
+      },
+      {
+        Product: "Chocolate",
+        kg: 0.025
+      },
+      {
+        Product: "Strawberries",
+        kg: 0.0375
+      },
+      {
+        Product: "Biscuits",
+        kg: 0.005
       }
     ]
   }
