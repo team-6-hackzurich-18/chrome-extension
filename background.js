27203fb73534e2914cc6e06b8afe1e268c2d4862
David Keller <dev@davidkeller.me>
2018-09-15 15:34:30 +0200

    Change icon color depending on normalized value

--- background.js
+++ background.js
@@ -1,1 +1,35 @@
-console.log("Hello world")
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.storage.sync.set({})
+// })
+
+chrome.webNavigation.onCompleted.addListener(function(details) {
+    // console.log("We're on Fooby at " + details.url)
+
+    chrome.browserAction.setIcon({path: "images/icon_3.png"})
+
+}, {
+    url: [
+        {urlPrefix : 'https://fooby.ch/en/recipes/14229/lasagne-al-forno'}
+    ]
+})
+
+// const knownUrls = [
+//     'fooby.ch/en/recipes/14229/lasagne-al-forno'
+// ]
+
+// function isKnownUrl(urlString) {
+//     const url = new URL(urlString)
+//     var hostPath = url.host + url.pathname
+//     for (var knwonUrl in knownUrls) {
+//         if (knwonUrl === hostPath) {
+//             return hostPath
+//         }
+//     }
+//     return null
+// }
+
+// TODO: Reset icon state when you exit a page
+// chrome.webNavigation.onBeforeNavigate.addListener(function() {
+//     console.log("reset")
+//     chrome.browserAction.setIcon({path: "images/icon.png"})
+// })
