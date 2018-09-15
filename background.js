43d9992d66fef7bd7d620c44e01d5c7f22d87a59
David Keller <dev@davidkeller.me>
2018-09-15 21:59:21 +0200

    Use API

--- background.js
+++ background.js
@@ -2,22 +2,64 @@
 //     chrome.storage.sync.set({})
 // })
 
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    // console.log("We're on Fooby at " + details.url)
+function getColor(worldEquivalent) {
+    var color = '#000000'
+    // 0.75 sehr grün
+    // 1 grün
+    // 1.25 gelb
+    // 1.5 rot
+    // > sehr rot
+    if (worldEquivalent < 0.75) {
+        color = '#417505'
+    } else if (worldEquivalent < 1) {
+        color = '#7ED321'
+    } else if (worldEquivalent < 1.25) {
+        color = '#F8E71C'
+    } else if (worldEquivalent < 1.5) {
+        color = '#F5A623'
+    } else {
+        color = '#D0021B'
+    }
 
-    chrome.browserAction.setIcon({path: "images/icon_3.png"})
+    return color
+}
+
+const colorToImageId = {
+    '#417505': 0,
+    '#7ED321': 1,
+    '#F5A623': 2,
+    '#F8E71C': 3,
+    '#D0021B': 4
+}
+
+function updateIcon(color, reset) {
+    var imagePath = "images/icon.png"
+
+    if (!reset) {
+        imagePath = "images/icon_" + colorToImageId[color] + ".png"
+    }
+
+    chrome.browserAction.setIcon({path: imagePath})    
+}
+
+function enhanceWebsite(info, ingredients) {
+    var worldEquivalent = ingredients["worldsConsumed"]
+    var co2 = ingredients["cost_recipe"]
+
+    var color = getColor(worldEquivalent)
+    updateIcon(color, false)
 
     chrome.tabs.executeScript(
-        details.tabId,
+        info.tabId,
         { 
             code: `
                 var metaContainer = document.getElementsByClassName('page-header-recipe__meta-container')[0]
 
-                var informationNode =   \`<div class="meta-info meta-info--big" style="color: orange;">
-                                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512" style="fill: orange">
+                var informationNode =   \`<div class="meta-info meta-info--big" style="color: `+ color +`;">
+                                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512" style="fill: `+ color +`">
                                                 <path d="M256 0c-141.385 0-256 114.615-256 256s114.615 256 256 256 256-114.615 256-256-114.615-256-256-256zM256 480.001c-31.479 0-61.436-6.506-88.615-18.226l116.574-131.145c2.603-2.929 4.041-6.711 4.041-10.63v-48c0-8.837-7.163-16-16-16-56.495 0-116.102-58.731-116.687-59.313-3-3.001-7.070-4.687-11.313-4.687h-64c-8.836 0-16 7.164-16 16v96c0 6.061 3.424 11.601 8.845 14.311l55.155 27.578v93.943c-58.026-40.478-96-107.716-96-183.832 0-34.357 7.745-66.903 21.569-96h58.431c4.244 0 8.313-1.686 11.314-4.686l64-64c3-3.001 4.686-7.070 4.686-11.314v-38.706c20.281-6.037 41.759-9.294 64-9.294 35.203 0 68.502 8.13 98.141 22.6-2.072 1.751-4.088 3.582-6.023 5.518-18.133 18.132-28.118 42.239-28.118 67.882s9.985 49.75 28.118 67.882c18.217 18.216 42.609 28.132 67.817 28.13 1.583 0 3.171-0.040 4.759-0.118 6.907 25.901 19.376 93.328-4.202 186.167-0.222 0.872-0.348 1.744-0.421 2.612-40.662 41.54-97.35 67.328-160.071 67.328z"></path>
                                             </svg>
-                                            Emissions: 304.3 kg CO2
+                                            Emissions: ` + co2.toFixed(2) + ` kg CO<sub>2</sub>
                                         </div>\`
                 
                 metaContainer.innerHTML = informationNode + metaContainer.innerHTML
@@ -47,20 +89,54 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
 
                 document.getElementById("add_to_mealplan").addEventListener("click",
                 function() {
-                    chrome.runtime.sendMessage('Test message!')
+                    chrome.runtime.sendMessage('` + info.key + `')
                 }, false);
             `
         }
     )
+}
+
+chrome.webNavigation.onCompleted.addListener(function(details) {
+    // console.log("We're on Fooby at " + details.url)
+
+    var info = {
+        url: details.url,
+        tabId: details.tabId
+    }
+
+    if (axiosInstance == null) {
+        initalizeAxiosInstance(info, getScoreForRecipe)
+    } else {
+        getScoreForRecipe(info)
+    }
 }, {
     url: [
-        {urlPrefix : 'https://fooby.ch/en/recipes/14229/lasagne-al-forno'}
+        {urlPrefix: 'https://fooby.ch/en/recipes/'}
     ]
 })
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     console.log("Got message: " + message)
+    var key = message
+    if (key in recipes) {
+        addRecipeToMealplan(recipes[key])
+    } else {
+        console.log("Error: Recipe not found for storing.")
+    }    
 })
+
+function addRecipeToMealplan(recipe) {
+    const restPath = '/recipes'
+    console.log(recipe)
+    axiosInstance.post(restPath, recipe)
+    .then(function(response) {
+        console.log("Successfuly added!")
+        console.log(response)
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+}
 
 // const knownUrls = [
 //     'fooby.ch/en/recipes/14229/lasagne-al-forno'
@@ -82,3 +158,41 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 //     console.log("reset")
 //     chrome.browserAction.setIcon({path: "images/icon.png"})
 // })
+
+// API
+
+var axiosInstance = null
+function initalizeAxiosInstance(params, callback) {
+    chrome.storage.sync.get('apiBaseUrl', function(data) {
+        axiosInstance = axios.create({
+            baseURL: data.apiBaseUrl
+        })
+        callback(params)
+    })
+}
+
+// We expect axiosInstance to be initalized
+function getScoreForRecipe(info) {
+    var link = new URL(info.url)
+    var key = link.host + link.pathname
+    info.key = key
+    if (key in recipes) {
+        getScoreForIngredients(info, recipes[key]["ingredients"])
+    } else {
+        // Maybe randomize
+        updateIcon(0, true)
+    }    
+}
+
+function getScoreForIngredients(info, ingredients) {
+    const restPath = '/stats/ingredients'
+    axiosInstance.post(restPath, {
+        ingredients: ingredients
+    })
+    .then(function(response) {
+        enhanceWebsite(info, response.data)
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+}
---
+++ meta/recipes.js
@@ -0,0 +1,52 @@
+const recipes = {
+  "fooby.ch/en/recipes/14229/lasagne-al-forno": {
+    "Name": "Lasagne",
+
+    "ingredients": [
+      {
+        "Product": "Beef",
+        "kg": 0.15
+      },
+      {
+        "Product": "Onions",
+        "kg": 0.044
+      },
+      {
+        "Product": "Tomatoes",
+        "kg": 0.015
+      },
+      {
+        "Product": "Carrots",
+        "kg": 0.05
+      },
+      {
+        "Product": "Celery",
+        "kg": 0.05
+      },
+      {
+        "Product": "Red wine",
+        "kg": 0.075
+      },
+      {
+        "Product": "Butter",
+        "kg": 0.012
+      },
+      {
+        "Product": "White Flour",
+        "kg": 0.01
+      },
+      {
+        "Product": "Whole Milk",
+        "kg": 0.15
+      },
+      {
+        "Product": "Dry Pasta",
+        "kg": 0.125
+      },
+      {
+        "Product": "Cheese slices",
+        "kg": 0.02
+      }
+    ]
+  }
+}
